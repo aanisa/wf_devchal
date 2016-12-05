ec30d04a9c9a3eb5f778314a7c0e51c9133beeb4
Dan Grigsby <dan@Dans-Macintosh.roc.mn.charter.com>
2016-12-04 19:27:44 -0600

    wipe and recreate DB every time a test is run

--- app/apply/models.py
+++ app/apply/models.py
@@ -30,15 +30,6 @@ class School(Base):
     email = db.Column(db.String(80))
     survey_monkey_choice_id = db.Column(db.String(80))
 
-    def XXX_HERE_TOD_survey_monkey_choice_id(self, survey_id, survey_monkey_which_schools_question_id):
-        for page in Survey(survey_id).data["pages"]:
-            for question in page["questions"]:
-                if question["id"] == survey_monkey_which_schools_question_id:
-                    for choice in question["answers"]["choices"]:
-                        if choice["text"].lower().find(self.match.lower()) >= 0:
-                            return choice["id"]
-        raise LookupError
-
 request_session = requests.session()
 request_session.headers.update({
   "Authorization": "Bearer {0}".format(app.config['SURVEY_MONKEY_OAUTH_TOKEN']),
@@ -49,6 +40,15 @@ class Survey():
     @lru_cache(maxsize=None)
     def __init__(self, id):
         self.data = request_session.get("https://api.surveymonkey.net/v3/surveys{0}/details".format(id)).json()
+
+    def survey_monkey_choice_id_for_school(self, school, survey_monkey_which_schools_question_id):
+        for page in Survey(survey_id).data["pages"]:
+            for question in page["questions"]:
+                if question["id"] == survey_monkey_which_schools_question_id:
+                    for choice in question["answers"]["choices"]:
+                        if choice["text"].lower().find(school.match.lower()) >= 0:
+                            return choice["id"]
+        raise LookupError
 
 @lru_cache(maxsize=None)
 def responses(id):
--- app/apply/tests.py
+++ app/apply/tests.py
@@ -4,11 +4,10 @@ from app import app, db
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        # TODO - delete all data from tables
-        pass
+        # reset db
+        db.reflect()
+        db.drop_all()
+        db.create_all()
 
     def test_foo(self):
-        c = models.School(name="Name")
-        db.session.add(c)
-        db.session.commit()
         assert True
