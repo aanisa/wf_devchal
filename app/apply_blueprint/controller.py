954d5a6b3388da1beaa53841dc718e9a6ef1b9ad
Dan Grigsby <dan@Dans-Macintosh.local>
2016-12-13 15:33:36 -0600

    use correct config

--- app/apply_blueprint/controller.py
+++ app/apply_blueprint/controller.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, request
 import models
+from app import app
 
 apply_blueprint = Blueprint('apply_blueprint', __name__, template_folder='templates')
 
 @apply_blueprint.route('/redirect_to_survey_monkey_with_guid')
 def redirect_to_survey_monkey_with_guid():
-    return render_template('redirect_to_survey_monkey_with_guid.html', survey_monkey_collector_id=apply_blueprint.config['SURVEY_MONKEY_COLLECTOR_ID'])
+    return render_template('redirect_to_survey_monkey_with_guid.html', survey_monkey_collector_id=app.config['SURVEY_MONKEY_COLLECTOR_ID'])
 
 @apply_blueprint.route('/after_survey_monkey')
 def after_survey_monkey():
