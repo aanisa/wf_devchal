6f4d4939477c24f4895c4c2843550ce8dbfc2a88
Dan Grigsby <dan@Dans-Macintosh.local>
2016-12-23 13:45:00 -0600

    hello world react

---
+++ app/apply_blueprint/react/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+setTimeout(function(){
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+}, 200);
