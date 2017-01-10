ffc306fa73250e3dacba9206627109795bfeb83b
Dan Grigsby <dan@Dans-Macintosh.local>
2017-01-10 15:22:54 -0600

    removed api

--- app/apply_blueprint/controller.py
+++ app/apply_blueprint/controller.py
@@ -23,21 +23,3 @@ def after_survey_monkey():
 def calendly_webhook():
     models.Appointment(request.get_json()).update_checklist()
     return "OK"
-
-api = Api(blueprint)
-
-class SchoolResource(Resource):
-    def get(self, tc_school_id, tc_api_token):
-        return models.SchoolSchema().jsonify(models.School.query.filter_by(tc_school_id=tc_school_id).first())
-
-api.add_resource(SchoolResource, '/school/<int:tc_school_id>/<string:tc_api_token>')
-
-class ChecklistResource(Resource):
-    def put(self, checklist_id):
-        checklist = models.Checklist.query.get(checklist_id)
-        for key, value in request.get_json().iteritems():
-            setattr(checklist, key, value)
-        db.session.commit()
-        return models.ChecklistSchema().jsonify(checklist)
-
-api.add_resource(ChecklistResource, '/checklist/<int:checklist_id>')
