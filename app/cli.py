143e7bae2eeab349f3fa726c8cec589ee98c30ce
Dan Grigsby <dan.grigsby@wildflowerschools.org>
2017-04-20 10:16:26 -0500

    added empty/commented out app level cli

---
+++ app/cli.py
@@ -0,0 +1,14 @@
+# from app import app
+# import os
+#
+# @app.cli.group(name = "generate")
+# def cli():
+#     """Generate"""
+#     pass
+#
+# @cli.command("blueprint")
+# @cli.argument("name")
+# def blueprint(name):
+#     """Generate empty blueprint"""
+#     print name
+# cli.add_command(blueprint)
