703056885a2472edb1a4377a8ccc194dd95057ce
Anisa <anisa.ali7@gmail.com>
2017-06-29 14:48:14 -0500

    fix format issue from beautify

--- app/apply_blueprint/react/app.js
+++ app/apply_blueprint/react/app.js
@@ -8,115 +8,93 @@ var scripts = document.getElementsByTagName("script");
 var thisUrl = scripts[scripts.length - 1].src;
 
 class UI extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { value: "", label: "Save", disabled: false };
-        var that = this;
-        fetch(thisUrl + "/../../email_template?tc_school_id=" + tc.env.currentSchoolId)
-            .then(function(response) {
-                if (response.status == 404) {
-                    fetch(thisUrl + "/../../email_template?tc_api_token=" + tc.env.userApiToken)
-                        .then(function(response) {
-                            return response.text();
-                        }).then(function(text) {
-                            that.setState({ value: text });
-                        }).catch(function(ex) {
-                            alert('failed:' + ex);
-                        })
-                } else {
-                    console.log('THE response', response);
-                    return response.text();
-                }
-            }).then(function(text) {
-                console.log('Text', text);
-                that.setState({ value: text });
-            }).catch(function(ex) {
-                alert('failed:' + ex);
-            })
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      label: "Save",
+      disabled: false
+    };
+    var that = this;
+    fetch(thisUrl + "/../../email_template?tc_school_id=" + tc.env.currentSchoolId).then(function(response) {
+      if (response.status == 404) {
+        fetch(thisUrl + "/../../email_template?tc_api_token=" + tc.env.userApiToken).then(function(response) {
+          return response.text();
+        }).then(function(text) {
+          that.setState({value: text});
+        }).catch(function(ex) {
+          alert('failed:' + ex);
+        })
+      } else {
+        console.log('THE response', response);
+        return response.text();
+      }
+    }).then(function(text) {
+      console.log('Text', text);
+      that.setState({value: text});
+    }).catch(function(ex) {
+      alert('failed:' + ex);
+    })
+  }
+  changed(e) {
+    this.setState({value: e.target.value});
+  }
+  save(e) {
+    this.setState({label: "Saving...", disabled: true})
+    var that = this;
+    fetch(thisUrl + "/../../email_template_post_parameters?tc_school_id=" + tc.env.currentSchoolId + "&tc_api_token=" + tc.env.userApiToken).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      var f = new FormData();
+      for (var k in json.fields) {
+        f.append(k, json.fields[k]);
+      }
+      var b = new Blob([that.state.value], {type: "text/html"});
+      f.append("file", b);
+      fetch(json.url, {
+        method: 'POST',
+        body: f
+      }).then(function(response) {
+        that.setState({label: "Saved"})
+        setTimeout(function() {
+          that.setState({label: "Save", disabled: false})
+        }, 3000)
+      }).catch(function(ex) {
+        alert('updating status failed:' + ex);
+      })
+    }).catch(function(ex) {
+      alert('failed:' + ex);
+    })
+  }
+  keyDown(e) {
+    if (e.metaKey && e.keyCode == 83) {
+      this.save(e);
+      e.preventDefault();
+      return false;
     }
-    changed(e) {
-        this.setState({ value: e.target.value });
-    }
-    save(e) {
-        this.setState({ label: "Saving...", disabled: true })
-        var that = this;
-        fetch(thisUrl + "/../../email_template_post_parameters?tc_school_id=" + tc.env.currentSchoolId + "&tc_api_token=" + tc.env.userApiToken)
-            .then(function(response) {
-                return response.json();
-            }).then(function(json) {
-                var f = new FormData();
-                for (var k in json.fields) {
-                    f.append(k, json.fields[k]);
-                }
-                var b = new Blob([that.state.value], { type: "text/html" });
-                f.append("file", b);
-                fetch(json.url, {
-                    method: 'POST',
-                    body: f
-                }).then(function(response) {
-                    that.setState({ label: "Saved" })
-                    setTimeout(function() {
-                        that.setState({ label: "Save", disabled: false })
-                    }, 3000)
-                }).catch(function(ex) {
-                    alert('updating status failed:' + ex);
-                })
-            }).catch(function(ex) {
-                alert('failed:' + ex);
-            })
-    }
-    keyDown(e) {
-        if (e.metaKey && e.keyCode == 83) {
-            this.save(e);
-            e.preventDefault();
-            return false;
-        }
-    }
-    render() {
-        return ( <
-            div >
-            <
-            b > Parent Email Template < /b><br/ >
-            <
-            textarea name = "template"
-            id = "template"
-            cols = "120"
-            rows = "20"
-            value = { this.state.value }
-            onChange = {
-                (e) => this.changed(e)
-            }
-            onKeyDown = {
-                (e) => this.keyDown(e)
-            } > < /textarea><br/ >
-            <
-            input type = "button"
-            value = { this.state.label }
-            onClick = {
-                (e) => console.log('here is state:', this.state)
-            }
-            disabled = { this.state.disabled }
-            /><br/ >
-            <
-            /div>
-        )
+  }
+  render() {
+    return (
+      <div>
+           <b>Parent Email Template</b><br/>
+           <textarea name="template" id="template" cols="120" rows="20" value={this.state.value} onChange={(e) => this.changed(e)} onKeyDown={(e) => this.keyDown(e)}></textarea><br/>
+           <input type="button" value={this.state.label} onClick={(e) => this.save(e)} disabled={this.state.disabled}/><br/>
+      </div>
+)
     }
 }
 
 setTimeout(function() {
-    if (location.pathname.endsWith('application/parent_email_template')) {
-        document.title = 'Application: Parent Email Template';
-        ReactDOM.render( <
-            UI / > ,
-            document.getElementById('foundation')
-        );
-    }
+    if (location.pathname.endsWith('application/parent_email_template ')) {
+ document.title = 'Application: Parent Email Template';
+    ReactDOM.render(< UI / >, document.getElementById('foundation'));}
 
-    if (location.pathname == '/s/' + tc.env.currentSchoolId + '/schools/' + tc.env.currentSchoolId) {
-        var div = document.createElement("div");
-        div.innerHTML = '<a href="/s/' + tc.env.currentSchoolId + '/networks/wf/application/parent_email_template">Online Application: Parent Email Template</a>'
-        var form = document.querySelector('#edit_school')
-        form.parentNode.insertBefore(div, form.nextSibling);
-    }
+  if (location.pathname == '/s/' + tc.env.currentSchoolId + '/schools/' + tc.env.currentSchoolId) {
+    var div = document.createElement("div");
+    div.innerHTML = '<a href="/s/' + tc.env.currentSchoolId + '/networks/wf/application/parent_email_template">Online Application: Parent Email Template</a>'
+    var form = document.querySelector('#edit_school')
+    form.parentNode.insertBefore(div, form.nextSibling);
+  }
 
-}, 200);
+},
+200);
